7dca737b400e613c4b74ee0b461bdb0f2e7628c7
Stephanie Guzman <sguzman247@gmail.com>
2022-10-31 12:31:15 -0500

    Create method to add new reservation to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,11 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
+  addReservation = (newReservation) => {
+    this.setState({reservations: [...this.state.reservations, newReservation] })
+  }
+
+  componentDidMount = () => {
     fetch('http://localhost:3001/api/v1/reservations')
       .then(res => res.json())
       .then(data => this.setState({ reservations: data }))
@@ -21,7 +25,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <Form />
+        <Form addReservation={this.addReservation}/>
         <ReservationsContainer reservations={this.state.reservations}/>
       </div>
     )
