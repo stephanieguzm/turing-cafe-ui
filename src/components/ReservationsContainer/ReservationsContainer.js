bde65112d0ed96098b453aa1698ba9e9da05a2de
Stephanie Guzman <sguzman247@gmail.com>
2022-10-31 11:27:15 -0500

    Add date and key props and import css

--- src/components/ReservationsContainer/ReservationsContainer.js
+++ src/components/ReservationsContainer/ReservationsContainer.js
@@ -1,8 +1,9 @@
 import React from "react"
 import Reservation from '../Reservation/Reservation'
+import './ReservationsContainer.css'
 
 const ReservationsContainer = ({ reservations }) => { 
-  
+  console.log('reservations', reservations)
   const reservationCards = reservations.map( reservation => {
     return (
       <Reservation 
@@ -10,14 +11,17 @@ const ReservationsContainer = ({ reservations }) => {
         name={reservation.name}
         number={reservation.number}
         time={reservation.time}
+        date={reservation.date}
+        key={reservation.id}
       />
     )
   })
 
   return (
-    {reservationCards}
+    <section className='resy-container'>
+      {reservationCards}
+    </section>
   )
 }
-
 
 export default ReservationsContainer
